6c6a056939cf7f9d00c6f3aa60c32b0e556da499
S Sai Seran <33454771+saiseran-s@users.noreply.github.com>
2018-05-14 15:04:22 +0530

    Create gsm calling.ino

---
+++ gsm calling.ino
@@ -0,0 +1,18 @@
+char phn[]="+917288819541";
+void setup() {
+  Serial.begin(9600);
+  delay(200);
+  Serial.println("AT");
+  delay(1000);
+  Serial.print("ATD");
+  Serial.println(phn);
+  delay(50000);
+  Serial.print("ATH");
+ 
+    
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
